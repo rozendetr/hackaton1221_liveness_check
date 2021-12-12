7dd129f2dd5835303abe92a7385eb50b41e3bcbf
rozendetr <rozendetr@bk.ru>
2021-12-12 09:14:23 +0300

    Spoof PC == real

--- webapp/src/pipeline.py
+++ webapp/src/pipeline.py
@@ -16,13 +16,13 @@ class NNPipeLine:
         self.logger = logger
         self.detector = detector
         self.spoof_classificator = spoof_classificator
-        self.setting = {"detect_face_threshold": 0.5,
-                        "min_face_size": 20}
+        self.setting = {"detect_face_threshold": 0.3,
+                        "min_face_size": 5}
 
     def predict_images(self, batch_imgs: list):
         batch_bboxes = self.detector(batch_imgs,
-                                     detector_threshold=self.setting.get("detect_face_threshold", 0.5),
-                                     min_size_object=self.setting.get("min_face_size", 10))
+                                     detector_threshold=self.setting.get("detect_face_threshold", 0.3),
+                                     min_size_object=self.setting.get("min_face_size", 5))
         if not batch_bboxes:
             return None
 
@@ -36,6 +36,8 @@ class NNPipeLine:
                 h_bbox = y_br-y_tl
                 frame_crop = frame[y_tl:y_br, x_tl:x_br]
                 class_id = self.spoof_classificator(frame_crop)
+                if class_id in [7]:
+                    class_id = 0
                 spoof_bbox = [x_tl, y_tl, x_br, y_br, class_id]
                 spoof_bboxes.append(spoof_bbox)
             batch_detections.append(spoof_bboxes)
